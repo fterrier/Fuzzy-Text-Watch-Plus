83cf136881e791e4a378a1b7138eb739d3b58750
Mattias Backlund <mattias.backlund@bisnode.com>
2016-01-18 12:08:54 +0100

    Adapt dutch language to new language data structure

--- src/lang-dutch.c
+++ src/lang-dutch.c
@@ -1,41 +1,43 @@
+#include "num2words.h"
+
 // Language strings for Dutch
-#include "lang-dutch.h"
+const Language LANG_DUTCH = {
+	.hours = {
+		"een",
+		"twee",
+		"drie",
+		"vier",
+		"vijf",
+		"zes",
+		"zeven",
+		"acht",
+		"negen",
+		"tien",
+		"elf",
+		"twaalf"
+	},
 
-const char* const HOURS_NL[] = {
-	"een",
-	"twee",
-	"drie",
-	"vier",
-	"vijf",
-	"zes",
-	"zeven",
-	"acht",
-	"negen",
-	"tien",
-	"elf",
-	"twaalf"
+	.phrases = {
+		"*$1 uur ",
+		"vijf over *$1 ",
+		"tien over *$1 ",
+		"kwart over *$1 ",
+		"tien voor half *$2 ",
+		"vijf voor half *$2 ",
+		"half *$2 ",
+		"vijf over half *$2 ",
+		"tien over half *$2 ",
+		"kwart voor *$2 ",
+		"tien voor *$2 ",
+		"vijf voor *$2 "
+	},
+
+	.greetings = {
+		"Goede- morgen ",
+		"Goede- dag ",
+		"Goede- avond ",
+		"Goede- nacht "
+	},
+
+	.connection_lost = "Waar is je telefoon"
 };
-
-const char* const PHRASES_NL[] = {
-	"*$1 uur ",
-	"vijf over *$1 ",
-	"tien over *$1 ",
-	"kwart over *$1 ",
-	"tien voor half *$2 ",
-	"vijf voor half *$2 ",
-	"half *$2 ",
-	"vijf over half *$2 ",
-	"tien over half *$2 ",
-	"kwart voor *$2 ",
-	"tien voor *$2 ",
-	"vijf voor *$2 "
-};
-
-  const char* const GREETINGS_NL[] = {
-    "Goede- morgen ",
-    "Goede- dag ",
-    "Goede- avond ",
-    "Goede- nacht "
-  };
-
-const char* const CONNECTION_LOST_NL = "Waar is je telefoon";
--- src/lang-dutch.h
+++ src/lang-dutch.h
@@ -1,7 +1,4 @@
 // Language strings for Dutch
 #pragma once
 
-extern const char* const HOURS_NL[12];
-extern const char* const PHRASES_NL[12];
-extern const char* const GREETINGS_NL[4];
-extern const char* const CONNECTION_LOST_NL;
+extern const Language LANG_DUTCH;
