a64bbe186f064ccb5791d538fae3572c8e33df4b
Mattias Backlund <mattias.backlund@bisnode.com>
2016-02-12 15:29:02 +0100

    Fix english language for new line length

--- src/lang-english.c
+++ src/lang-english.c
@@ -16,6 +16,7 @@ const Language LANG_ENGLISH = {
     "twelve"
   },
 
+#ifdef PBL_PLATFORM_CHALK
   .phrases = {
     "*$1 o'clock ",
     "five past *$1 ",
@@ -25,27 +26,41 @@ const Language LANG_ENGLISH = {
     "twenty five past *$1 ",
     "half past *$1 ",
     "twenty five to *$2 ",
-    "twenty to *$2 ",
+    "twenty  to *$2 ",
     "quarter to *$2 ",
     "ten to *$2 ",
     "five to *$2 "
   },
 
-#ifdef PBL_PLATFORM_CHALK
   .greetings = {
     "Good morning ",
-    "Good day ",
+    "Good  day ",
     "Good evening ",
     "Good night "
   },
 #else
+  .phrases = {
+    "*$1 o'clock ",
+    "five  past *$1 ",
+    "ten  past *$1 ",
+    "quarter past *$1 ",
+    "twenty past *$1 ",
+    "twenty five  past *$1 ",
+    "half  past *$1 ",
+    "twenty five to *$2 ",
+    "twenty  to *$2 ",
+    "quarter to *$2 ",
+    "ten to *$2 ",
+    "five to *$2 "
+  },
+
   .greetings = {
-    "Good mor- ning ",
-    "Good day ",
-    "Good even- ing ",
+    "Good  mor-  ning ",
+    "Good  day ",
+    "Good even-  ing ",
     "Good night "
   },
 #endif
 
-  .connection_lost = "Where is your phone? "
+  .connection_lost = "Where  is your phone? "
 };
