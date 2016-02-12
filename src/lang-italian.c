1146a67dafe34a75f0d4dce7f58d68543bd67e60
Mattias Backlund <mattias.backlund@bisnode.com>
2016-02-12 15:47:47 +0100

    Fix italian language for new line length

--- src/lang-italian.c
+++ src/lang-italian.c
@@ -38,16 +38,16 @@ const Language LANG_ITALIAN = {
   },
 #else
   .phrases = {
-    "*$1 in punto ",
-    "*$1 e cinque ",
+    "*$1 in  punto ",
+    "*$1 e  cinque ",
     "*$1 e dieci ",
     "*$1 e un quarto ",
     "*$1 e venti ",
     "*$1 e venti- cinque ",
     "*$1 e  mezza ",
-    "*$1 e trenta- cinque ",
+    "*$1 e  trenta- cinque ",
     "*$2 meno venti ",
-    "*$2 meno  un quarto ",
+    "*$2 meno  un  quarto ",
     "*$2 meno dieci ",
     "*$2 meno cinque "
   },
@@ -60,6 +60,10 @@ const Language LANG_ITALIAN = {
     "Buona notte "
   },
 
+#ifdef PBL_PLATFORM_CHALK
   .connection_lost = "Dov'è il tuo tele- fono? "
+#else
+  .connection_lost = "Dov'è il tuo  tele- fono? "
+#endif
 
 };
